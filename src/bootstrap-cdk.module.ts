cc06e811cd9499f467dcf65aa2c3bc0a17ea26c8
Rafal Galka <rafal.galka@modernweb.pl>
2019-08-22 11:40:54 +0200

    chore(package): yarn lib:build

--- src/bootstrap-cdk.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-
-import { BsTableModule } from './table';
-
-const MODULES = [BsTableModule];
-
-@NgModule({ imports: MODULES, exports: MODULES })
-export class BootstrapCdkModule {
-}
--- src/public-api.ts
+++ src/public-api.ts
@@ -2,6 +2,4 @@
  * Public API Surface of bootstrap-cdk
  */
 
-export * from './table';
-
-export * from './bootstrap-cdk.module';
+export * from './table/index';
